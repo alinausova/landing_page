b47bd3d726a3d7e3f6971cf46e5e5f72198984cf
Alina Usova <alina.usova@motius.de>
2021-01-29 14:17:31 +0100

    public changes

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,7 +3,8 @@ import { useSpring, animated } from 'react-spring'
 import './styles.css'
 
 const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
+const trans = (x, y, s) => ` scale(${s})`
+//`perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 export default function Card() {
   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
